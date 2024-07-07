97c4cc9218abbd367d59a15a0734de6aff4d5cef
Meg <marinaadasilva3456@gmail.com>
2024-07-07 16:45:25 -0300

    fix: Resolvi alguns problemas e os arquivos foram movidos para facilitar o deploy

--- Superpad/assets/app.js
+++ Superpad/assets/app.js
@@ -7,49 +7,118 @@ const botaoSeis = document.querySelector("#seis");
 const botaoSete = document.querySelector("#sete");
 const botaoOito = document.querySelector("#oito");
 const botaoNove = document.querySelector("#nove");
+const container = document.querySelector(".container");
 
-const audioDo = new Audio("assets/Sons/1-dó.mp3");
-const audioRé = new Audio("assets/Sons/1-ré.mp3");
-const audioMi = new Audio("assets/Sons/1-mi.mp3");
-const audioFá = new Audio("assets/Sons/1-fá.mp3");
-const audioSol = new Audio("assets/Sons/1-sol.mp3");
-const audioLá = new Audio("assets/Sons/1-lá.mp3");
-const audioSi = new Audio("assets/Sons/1-si.mp3");
-const audioC = new Audio("assets/Sons/2-dó.mp3");
-const audioD = new Audio("assets/Sons/2-ré.mp3");
-
-botaoUm.addEventListener("click", function () {
-  audioDo.play();
+botaoUm.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1do.mp3");
+  audio.play();
 });
 
-botaoDois.addEventListener("click", function () {
-  audioRé.play();
+botaoDois.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1re.mp3");
+  audio.play();
 });
 
-botaoTres.addEventListener("click", function () {
-  audioMi.play();
+botaoTres.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1mi.mp3");
+  audio.play();
 });
 
-botaoQuatro.addEventListener("click", function () {
-  audioFá.play();
+botaoQuatro.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1fa.mp3");
+  audio.play();
 });
 
-botaoCinco.addEventListener("click", function () {
-  audioSol.play();
+botaoCinco.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1sol.mp3");
+  audio.play();
 });
 
-botaoSeis.addEventListener("click", function () {
-  audioLá.play();
+botaoSeis.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1la.mp3");
+  audio.play();
 });
 
-botaoSete.addEventListener("click", function () {
-  audioSi.play();
+botaoSete.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/1si.mp3");
+  audio.play();
 });
 
-botaoOito.addEventListener("click", function () {
-  audioC.play();
+botaoOito.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/2do.mp3");
+  audio.play();
 });
 
-botaoNove.addEventListener("click", function () {
-  audioD.play();
+botaoNove.addEventListener("click", () => {
+  const audio = new Audio("assets/Sons/2re.mp3");
+  audio.play();
+});
+
+container.addEventListener("keypress", (event) => {
+  const tecla = event.key;
+
+  switch (tecla) {
+    case "s":
+      botaoUm.click();
+      botaoUm.style.backgroundColor = "#d144b4";
+      setTimeout(() => {
+        botaoUm.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "d":
+      botaoDois.click();
+      botaoDois.style.backgroundColor = "#4d66ec";
+      setTimeout(() => {
+        botaoDois.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "j":
+      botaoTres.click();
+      botaoTres.style.backgroundColor = "#d144b4";
+      setTimeout(() => {
+        botaoTres.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "x":
+      botaoQuatro.click();
+      botaoQuatro.style.backgroundColor = "#4995ed";
+      setTimeout(() => {
+        botaoQuatro.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "c":
+      botaoCinco.click();
+      botaoCinco.style.backgroundColor = "#60d6f8";
+      setTimeout(() => {
+        botaoCinco.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "k":
+      botaoSeis.click();
+      botaoSeis.style.backgroundColor = "#4892ea";
+      setTimeout(() => {
+        botaoSeis.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "v":
+      botaoSete.click();
+      botaoSete.style.backgroundColor = "#9130c9";
+      setTimeout(() => {
+        botaoSete.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "n":
+      botaoOito.click();
+      botaoOito.style.backgroundColor = "#4d66ec";
+      setTimeout(() => {
+        botaoOito.style.backgroundColor = "inherit";
+      }, 200);
+      break;
+    case "m":
+      botaoNove.click();
+      botaoNove.style.backgroundColor = "#9130c9";
+      setTimeout(() => {
+        botaoNove.style.backgroundColor = "inherit";
+      }, 200);
+  }
 });
